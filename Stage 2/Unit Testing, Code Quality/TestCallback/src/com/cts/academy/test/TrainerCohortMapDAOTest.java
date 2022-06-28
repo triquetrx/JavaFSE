5b290be6b5ed08fb4079cf89d10de446032fe565
Zaid Khan <zdkhan0786@gmail.com>
2022-06-28 18:30:47 +0530

    Add files via upload

---
+++ Stage 2/Unit Testing, Code Quality/TestCallback/src/com/cts/academy/test/TrainerCohortMapDAOTest.java
@@ -0,0 +1,86 @@
+package com.cts.academy.test;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+import com.cts.academy.dao.TrainerCohortMapDAO;
+import com.cts.academy.model.Cohort;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TrainerCohortMapDAOTest {
+
+	//Mock and rule
+	@Mock
+	TrainerCohortMapDAO dao;
+	@Mock
+	Cohort cohort;
+	@Rule
+	public ExpectedException exceptionRule = ExpectedException.none();
+	
+
+	@Before
+	public void setUp() throws Exception {
+		
+		Mockito.doAnswer(new Answer<Void>() {
+			public Void answer(InvocationOnMock invocation) throws Throwable {
+				//Code here...
+				Object[] args = invocation.getArguments();
+				String trainerId = (String) args[0];
+				String cohortCode = (String) args[1];
+				if(!trainerId.startsWith("t-")) {
+					throw new RuntimeException("Invalid Trainer ID");
+				}
+				if(!cohortCode.matches("^[A-Z]{3}[0-9]{2}[A-Z]{2}[0-9]{3}")) {
+					throw new RuntimeException("Invalid Cohort Code");
+				}
+				cohort.setCohortCode(cohortCode);
+				cohort.setTrainerId(trainerId);
+				return null;
+			}
+		}).when(dao).mapTrainerCohort(any(String.class), any(String.class));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSuccess() {
+		try {
+			dao.mapTrainerCohort("t-Jerry", "CHN00AJ000");
+		}catch(Exception ex) {
+			
+		}
+		//code here
+		verify(cohort).setTrainerId("t-Jerry");
+	}
+
+	@Test
+	public void testTrainerId() {
+		//code here
+		exceptionRule.expect(RuntimeException.class);
+		exceptionRule.expectMessage("Invalid Trainer ID");
+		dao.mapTrainerCohort("Jerry", "CHN19AJ021");
+	}
+	
+	@Test
+	public void testCohortCode() {
+		//code here
+		exceptionRule.expect(RuntimeException.class);
+		exceptionRule.expectMessage("Invalid Cohort Code");
+		dao.mapTrainerCohort("t-Jerry", "AJ021");
+	}
+}
+
