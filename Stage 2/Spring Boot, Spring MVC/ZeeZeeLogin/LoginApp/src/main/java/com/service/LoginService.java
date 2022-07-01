cfd4737a0c8908d2ac5986941e74f345b7532ef5
Zaid Khan <zdkhan0786@gmail.com>
2022-07-01 10:17:51 +0530

    Add files via upload

---
+++ Stage 2/Spring Boot, Spring MVC/ZeeZeeLogin/LoginApp/src/main/java/com/service/LoginService.java
@@ -0,0 +1,16 @@
+package com.service;
+
+import org.springframework.stereotype.Service;
+
+import com.bean.LoginBean;
+
+@Service
+public class LoginService {
+	
+	public boolean validate(LoginBean bean)	{
+		
+		return bean.getUserName().equalsIgnoreCase(bean.getPassword());
+	}
+	
+
+}
